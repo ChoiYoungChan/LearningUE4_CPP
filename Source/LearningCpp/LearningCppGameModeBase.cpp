d5e0ea2cabcee8683981452b8b08b822e8fdfb24
ChoiYoungChan <40765022+ChoiYoungChan@users.noreply.github.com>
2022-08-23 22:54:29 +0900

    Learn UMG

--- Source/LearningCpp/LearningCppGameModeBase.cpp
+++ Source/LearningCpp/LearningCppGameModeBase.cpp
@@ -1,5 +1,31 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
+// UE4는 한번에 여러개의 위젯을 표시하고, 상호작용처리가 가능, 한개씩 제거할 수 있다.
+// 위젯을 직접 소멸시킬 필요는 없는데 뷰포트의 제거및 레퍼런싱하는 변수 제거는 UE4의 가비지컬렛견 시스템이 처리
 
 #include "LearningCppGameModeBase.h"
+#include <Blueprint/UserWidget.h>
 
+void ALearningCppGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+
+}
+
+void ALearningCppGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> _NewWidgetClass)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	if (_NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget(GetWorld(), _NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
--- Source/LearningCpp/LearningCppGameModeBase.h
+++ Source/LearningCpp/LearningCppGameModeBase.h
@@ -13,5 +13,17 @@ UCLASS()
 class LEARNINGCPP_API ALearningCppGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public :
+	// TSubclassOf -> UE4에서 Uclass안정성을 보장하는 템플릿 클래스
+		UFUNCTION(BlueprintCallable, Category = "UMG_Game")
+		void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UMG_Game")
+		TSubclassOf<UUserWidget> StartingWidgetClass;
+
+	UPROPERTY()
+		UUserWidget* CurrentWidget;
 };
